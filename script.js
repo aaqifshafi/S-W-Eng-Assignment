c88a9c04071196e3db0450d67e99219ce2312bd8
aaqifshafi <aaqifshafi@gmail.com>
2023-02-26 20:52:28 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function handleClick(id) {
+    switch (id) {
+        case "topic_1": {
+            window.location.href = "/topics/topic_1.html";
+            break;
+        }
+        case "topic_2": {
+            window.location.href = "/topics/topic_2.html";
+            break;
+        }
+        case "topic_3": {
+            window.location.href = "/topics/topic_3.html";
+            break;
+        }
+        case "topic_4": {
+            window.location.href = "/topics/topic_4.html";
+            break;
+        }
+        case "topic_5": {
+            window.location.href = "/topics/topic_5.html";
+            break;
+        }
+        case "topic_6": {
+            window.location.href = "/topics/topic_6.html";
+            break;
+        }
+        case "topic_7": {
+            window.location.href = "/topics/topic_7.html";
+            break;
+        }
+        case "topic_8": {
+            window.location.href = "/topics/topic_8.html";
+            break;
+        }
+        default: {
+            window.location.href = "/index.html";
+            break;
+        }
+    }
+}
